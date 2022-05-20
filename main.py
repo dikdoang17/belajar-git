09c27dfea58b60b76d652da72bbd31fbb7c1229d
dikdoang17 <dikafadhlan@gmail.com>
2022-05-20 20:08:01 +0700

    add file1.txt

--- main.py
+++
@@ -1,7 +0,0 @@
-def penjumlahan(a,b):
-    return total
-
-def main():
-   print penjumlahan(5,1)
-
-main()
--- pembagian.py
+++
@@ -1,7 +0,0 @@
-def pembagian(a,b):
-  	return total
-
-def main():
-	print pembagian(2,2)
-	
-main()
